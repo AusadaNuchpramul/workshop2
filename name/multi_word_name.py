935c63d8a4777be3f29fec166c303a6472a012ee
AusadaNuchpramul <ausada.n@ku.th>
2021-01-10 11:09:17 +0700

    Add new folder name and new 3 file

---
+++ name/multi_word_name.py
@@ -0,0 +1,8 @@
+#Camel Case 
+myVarialeName = "Ausada nuchpramul"
+
+#Pascal Case
+MyVarialeName = "Ausada nuchpramul"
+
+#Snake Case = python
+my_variable_name = "Ausada nuchpramul"
---
+++ name/name.py
@@ -0,0 +1,11 @@
+#สามารถประกาศได้
+myvar = "Aus"
+my_var = "Aus"
+_my_var = "Aus"
+myVar = "Aus"
+MYVAR = "Aus"
+myvar2 = "Aus"
+#ไม่สามารถประกาศได้
+2myvar = "Aus"
+my-var = "Aus"
+my var = "Aus"
